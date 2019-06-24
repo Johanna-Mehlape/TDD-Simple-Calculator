413cdbafb331c557324c4fd4a18177463362924b
Johanna-Mehlape <johanna.mehlape@umuzi.org>
2019-06-24 09:39:58 +0200

    Changes Commited

---
+++ string calculator/calculator.py
@@ -0,0 +1,12 @@
+def add(*args):
+        sum = 0
+        for i in args:
+                sum += i
+        return sum
+
+
+def multiply(*args):
+        result = 1
+        for arg in args:
+                result *= arg
+        return result
